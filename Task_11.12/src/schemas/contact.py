2c8a44f383930df8f3b1966b09cc51b707ce481d
Oleksii Neshcheret <85193924+Kunandiir@users.noreply.github.com>
2024-04-01 19:04:45 +0200

    Task_12 completed

--- Task_11.12/src/schemas/contact.py
+++ Task_11.12/src/schemas/contact.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, EmailStr, Field
-from datetime import date
+from datetime import date, datetime
+from src.schemas.user import UserResponse, UserSchema, TokenSchema
 
 
 class ContactSchema(BaseModel):
@@ -21,6 +22,9 @@ class ContactResponse(BaseModel):
     email: EmailStr 
     phone_number: str 
     birthday: date 
+    created_at: datetime | None
+    updated_at: datetime | None
+    #user: UserResponse | None
 
     class Config:
         from_attributes = True
